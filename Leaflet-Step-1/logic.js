cc73045695fdf64458af324bc1aab0a01ff562be
Ellen White <ellenwhite@macbook-air-4.attlocal.net>
2022-04-15 11:03:39 -0500

    done

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -10,17 +10,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Retrieve earthquakesURL (USGS Earthquakes GeoJSON Data) with D3
+
 d3.json(url).then(function(earthquakeData) {
     console.log(earthquakeData)
-    // Function to Determine Size of Marker Based on the Magnitude of the Earthquake
-    
     
     function markerSize(magnitude) {
         if (magnitude === 0) return 1;
         else return magnitude * 2;
     }
-    // Function to Determine Style of Marker Based on the Magnitude of the Earthquake
+    function chooseColor(depth) {
+        if (depth > 50) return "firebrick";
+        else if (depth > 40) return "red";
+        else if (depth > 30) return "orange";
+        else if (depth > 20) return "yellow";
+        else if (depth > 10) return "green";
+        else return "greenyellow";
+        }
+    // marker size/color based on depth/mag
     function styleInfo(feature) {
         return {
           opacity: 1,
@@ -31,32 +37,23 @@ d3.json(url).then(function(earthquakeData) {
           weight: 0.5
         };
     }
-    // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
-    function chooseColor(depth) {
-        if (depth > 50) return "firebrick";
-        else if (depth > 40) return "red";
-        else if (depth > 30) return "orange";
-        else if (depth > 20) return "yellow";
-        else if (depth > 10) return "green";
-        else return "greenyellow";
-        }
+   
     
-    // Create a GeoJSON Layer Containing the Features Array on the earthquakeData Object
     L.geoJSON(earthquakeData, {
         pointToLayer: function(feature, latlng) {
             return L.circleMarker(latlng);
         },
         style: styleInfo,
-        // Function to Run Once For Each feature in the features Array
-        // Give Each feature a Popup Describing the Place & Time of the Earthquake
+        //popup
         onEachFeature: function(feature, layer) {
             layer.bindPopup("<h4>Location: " + feature.properties.place + 
             "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
             "</p><hr><p>Magnitude: " + feature.properties.mag + 
             "</p><hr><p>Depth: " + feature.geometry.coordinates[2] + "</p>");
         }
-    // Add earthquakeData to earthquakes LayerGroups 
     }).addTo(myMap);
+
+    // legend
     var legend = L.control({ position: "topright" });
     legend.onAdd = function() {
         var div = L.DomUtil.create("div", "info legend"), 
@@ -71,6 +68,6 @@ d3.json(url).then(function(earthquakeData) {
         }
         return div;
     };
-    // Add Legend to the Map
+    
     legend.addTo(myMap);
 });
